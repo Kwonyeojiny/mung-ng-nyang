339dce85703fa8b23e84c6121e1ec6efe13e0fb8
Kwonyeojiny <kangagi0009@naver.com>
2024-10-21 23:05:59 +0900

    📦️ Chore:라이브러리 추가

--- mung-ng-nyang/src/App.jsx
+++ mung-ng-nyang/src/App.jsx
@@ -1,11 +1,22 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import SignUp from "./pages/signUp";
 
 const App = () => {
   
 
   return (
     <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
       {/* 페이지들 여기에 담아줄거임???*/}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Login" element={<Login/>}/>
+          <Route path="/SignUp" element={<SignUp/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
