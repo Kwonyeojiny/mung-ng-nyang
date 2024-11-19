631d60dd36a244477f53251eb1b0b78cc2679fb0
soheekimdev <soheekim.dev@gmail.com>
2024-11-19 16:52:39 +0900

    Chore: 프로젝트 폴더 구조 변경, App.jsx 라우팅 구조 변경
    - 최상단 폴더 제거
    - App.jsx: Layout 컴포넌트 연결

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Login from './pages/login';
+import SignUp from './pages/signUp';
+import MyPage from './pages/myPage/MyPage';
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/mypage" element={<MyPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
