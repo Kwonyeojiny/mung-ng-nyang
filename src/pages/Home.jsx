77f620f15d13cad4ac12c7e384e204451292c829
Kwonyeojiny <kangagi0009@naver.com>
2024-11-23 23:18:05 +0900

    Feat: 폰트 설정 및 네브바, copyright Layout 컴포넌트 생성

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="flex justify-center items-center">home</div>
+    </Layout>
+  );
+};
+
+export default Home;
